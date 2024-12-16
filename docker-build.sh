c889d781e5f957f1819553402a33577f1f56fae0
Yassine Yousfi <yyousfi1@binghamton.edu>
2024-12-16 15:50:12 -0800

    with nvml

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ if [ ! -d slurm ]; then
     wget -O slurm-latest.tar.bz2 $(wget -qO- https://www.schedmd.com/downloads.php | grep -oP 'href="\K[^"]+\.tar\.bz2' | head -1)
     tar xvjf slurm-latest.tar.bz2
     mv "$(ls -d slurm-*/ | head -1)" slurm
-    sed -i '/^\s*dh_auto_configure/s/$/ --with-mysql_config=\/usr\/bin --with-munge=no/' slurm/debian/rules
+    sed -i '/^\s*dh_auto_configure/s/$/ --with-mysql_config=\/usr\/bin --with-munge=no --with-nvml/' slurm/debian/rules
     sed -i '/munge/d' slurm/debian/control
 fi
 
